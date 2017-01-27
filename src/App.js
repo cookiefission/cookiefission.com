55207da1d14c34d787f4da5bc2031b9144e53729
Sean Kenny <sean.kenny@hotmail.com>
2017-01-27 21:14:25 +0000

    Change React logo to be my ugly mug instead

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import profile from './profile.jpg';
 import 'font-awesome/css/font-awesome.css'
 
 import ExternalLinks from './ExternalLinks';
@@ -11,8 +11,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello, I'm Sean</h2>
+          <img src={profile} className="App-picture" alt="that's me that is" />
+          <h2>Hi, I'm Sean</h2>
         </div>
         <div className="App-intro">
           <p>
