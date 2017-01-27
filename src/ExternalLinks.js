6ed33e66d9a4bd9dd60c83b41c5019b785be0075
Sean Kenny <sean.kenny@hotmail.com>
2017-01-27 20:35:47 +0000

    Add styling to external links
    This makes them spread correctly rather than bunching up

--- src/ExternalLinks.js
+++ src/ExternalLinks.js
@@ -4,6 +4,8 @@ import GithubLink from './links/GithubLink';
 import TwitterLink from './links/TwitterLink';
 import MediumLink from './links/MediumLink';
 
+import './ExternalLinks.css';
+
 const ExternalLinks = () => {
   return(
     <div className='ExternalLinks' >
